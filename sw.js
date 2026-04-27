89bae781884dd6377d77269f25ee7b6e987c8853
atizoun84 <atizoun@gmail.com>
2026-04-27 19:58:45 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'designateur-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/logoapp.png',
+  './',
+  './index.html',
+  './manifest.json',
+  './logoapp.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
